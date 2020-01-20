b87fb12723545a7312f7534c4f5c85aa2ff57304
pengmiao <paungmiao@163.com>
2020-01-20 18:36:52 +0800

    rest远程调用配置类

---
+++ xjcloud-provider/xjcloud-provider-audit/src/main/java/gov/pbc/xjcloud/provider/contract/config/FeignClientConfig.java
@@ -0,0 +1,40 @@
+package gov.pbc.xjcloud.provider.contract.config;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+/**
+ * @author paungmiao@163.com
+ * @date 2020年1月20日16:27:00
+ */
+@Component
+public class FeignClientConfig implements RequestInterceptor {
+
+    @Override
+    public void apply(RequestTemplate template) {
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        if (requestAttributes == null) {
+            return;
+        }
+        HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+        Enumeration<String> headerNames = request.getHeaderNames();
+        if (headerNames != null) {
+            while (headerNames.hasMoreElements()) {
+                String name = headerNames.nextElement();
+                Enumeration<String> values = request.getHeaders(name);
+                while (values.hasMoreElements()) {
+                    String value = values.nextElement();
+                    System.out.println(name + "---" + value);
+                    template.header(name, value);
+                }
+            }
+        }
+    }
+}
