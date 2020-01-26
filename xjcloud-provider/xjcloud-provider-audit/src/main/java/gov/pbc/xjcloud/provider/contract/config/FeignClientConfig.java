31dab486ebeb047e13138b3911e1740be34fc70d
paungmiao@163.com <ling520!>
2020-01-26 10:54:24 +0800

    zuul配置修改

--- xjcloud-provider/xjcloud-provider-audit/src/main/java/gov/pbc/xjcloud/provider/contract/config/FeignClientConfig.java
+++ xjcloud-provider/xjcloud-provider-audit/src/main/java/gov/pbc/xjcloud/provider/contract/config/FeignClientConfig.java
@@ -2,6 +2,11 @@ package gov.pbc.xjcloud.provider.contract.config;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import lombok.var;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -14,8 +19,15 @@ import java.util.Enumeration;
  * @author paungmiao@163.com
  * @date 2020年1月20日16:27:00
  */
-@Component
+@Configuration
 public class FeignClientConfig implements RequestInterceptor {
+
+    @Value("${audit.token-name:Authorization}")
+    private String tokenName;
+
+    public FeignClientConfig() {
+
+    }
 
     @Override
     public void apply(RequestTemplate template) {
@@ -24,17 +36,22 @@ public class FeignClientConfig implements RequestInterceptor {
             return;
         }
         HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
-        Enumeration<String> headerNames = request.getHeaderNames();
-        if (headerNames != null) {
-            while (headerNames.hasMoreElements()) {
-                String name = headerNames.nextElement();
-                Enumeration<String> values = request.getHeaders(name);
-                while (values.hasMoreElements()) {
-                    String value = values.nextElement();
-                    System.out.println(name + "---" + value);
-                    template.header(name, value);
-                }
-            }
+        String headerValue = request.getHeader(tokenName);
+        if (StringUtils.isBlank(headerValue)) {
+            throw new IllegalAccessError("无法获取认证信息");
         }
+        template.header(tokenName, headerValue);
+//        Enumeration<String> headerNames = request.getHeaderNames();
+//        if (headerNames != null) {
+//            while (headerNames.hasMoreElements()) {
+//                String name = headerNames.nextElement();
+//                Enumeration<String> values = request.getHeaders(name);
+//                while (values.hasMoreElements()) {
+//                    String value = values.nextElement();
+//                    System.out.println(name + "---" + value);
+//                    template.header(name, value);
+//                }
+//            }
+//        }
     }
 }
