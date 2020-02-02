3c367e27982260e84a1acf3118a3cac564cbb225
lizhuxiong123@163.com <lizhuxiong123@163.com>
2020-02-02 13:40:31 +0800

    020201

--- xjcloud-provider/xjcloud-provider-audit/src/main/java/gov/pbc/xjcloud/provider/contract/feign/activiti/AuditActivitiService.java
+++ xjcloud-provider/xjcloud-provider-audit/src/main/java/gov/pbc/xjcloud/provider/contract/feign/activiti/AuditActivitiService.java
@@ -3,17 +3,32 @@ package gov.pbc.xjcloud.provider.contract.feign.activiti;
 import gov.pbc.xjcloud.common.core.util.R;
 import gov.pbc.xjcloud.provider.contract.config.FeignClientConfig;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 /**
- * 远程调用
+ * 远程调用activity
  */
 @FeignClient(value = "xjcloud-provider-activiti",configuration = FeignClientConfig.class )
 public interface AuditActivitiService {
 
-    @PutMapping("/process/start/{processDefKey}/{businessId}")
-    R start(@PathVariable("processDefKey") String processDefKey, @PathVariable("businessId") Integer businessId, @RequestParam(value = "vars") String vars);
+    @PutMapping({"/process/start/{processDefKey}/{businessId}"})
+    R start(@PathVariable("processDefKey") String paramString1, @PathVariable("businessId") Integer paramInteger, @RequestParam("vars") String paramString2);
+
+    @GetMapping({"/task/todo/{processDefKey}"})
+    R todo(@PathVariable("processDefKey") String paramString, @RequestParam Map<String, Object> paramMap);
+
+    @PostMapping({"/task/{taskId}"})
+    R complete(@PathVariable("taskId") String paramString1, @RequestParam("vars") String paramString2);
+
+    @DeleteMapping({"/task/{processDefKey}/{businessId}"})
+    R cancel(@PathVariable("processDefKey") String paramString1, @PathVariable("businessId") String paramString2);
+
+    @GetMapping({"/task/history/{processDefKey}/{businessId}"})
+    R history(@PathVariable("processDefKey") String paramString1, @PathVariable("businessId") String paramString2);
+
+    @GetMapping({"/process/var/{processDefKey}/{businessId}/{varName}"})
+    R getVariable(@PathVariable("processDefKey") String paramString1, @PathVariable("businessId") String paramString2, @PathVariable("varName") String paramString3);
 
 }
