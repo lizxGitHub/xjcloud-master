13a40eb4386105010cd7bdd6bdb9366b226917b6
paungmiao@163.com <ling520!>
2020-02-05 02:44:03 +0800

    1、更新延期申请相关操作

--- xjcloud-provider/xjcloud-provider-audit/src/main/java/gov/pbc/xjcloud/provider/contract/controller/auditManage/TaskController.java
+++ xjcloud-provider/xjcloud-provider-audit/src/main/java/gov/pbc/xjcloud/provider/contract/controller/auditManage/TaskController.java
@@ -16,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -47,7 +48,7 @@ public class TaskController {
      * @return
      */
     @GetMapping("todo")
-    public R<Map<String,Object>> LustToDo(Map<String, Object> params) {
+    public R<Map<String, Object>> LustToDo(Map<String, Object> params) {
         try {
             if (!params.containsKey("size")) {
                 params.put("size", 10);
@@ -55,16 +56,16 @@ public class TaskController {
             if (!params.containsKey("current")) {
                 params.put("current", 1);
             }
-            R<LinkedHashMap<String,Object>> actTaskMap = activitiService.todo(auditFlowDefKey, params);
-            LinkedHashMap<String,Object> actTaskMapData = actTaskMap.getData();
-            List<ActAuditVO> resultList = ((List<Map>)actTaskMapData.get("records")).stream().filter(Objects::nonNull).map(actVO -> {
+            R<LinkedHashMap<String, Object>> actTaskMap = activitiService.todo(auditFlowDefKey, params);
+            LinkedHashMap<String, Object> actTaskMapData = actTaskMap.getData();
+            List<ActAuditVO> resultList = ((List<Map>) actTaskMapData.get("records")).stream().filter(Objects::nonNull).map(actVO -> {
                 String actJsonStr = JSONObject.toJSONString(actVO);
                 ActAuditVO actAuditVO = JSONUtil.toBean(actJsonStr, ActAuditVO.class);
                 R<Integer> auditStatus = activitiService.getTaskVariable(actAuditVO.getTaskId(), "auditStatus");
                 actAuditVO.setAuditStatus(auditStatus.getData());
                 return actAuditVO;
             }).collect(Collectors.toList());
-            actTaskMap.getData().put("records",resultList);
+            actTaskMap.getData().put("records", resultList);
             return new R().setData(actTaskMap);
         } catch (Exception e) {
             e.printStackTrace();
@@ -85,9 +86,27 @@ public class TaskController {
             //修改状态
             String planId = (String) params.get("bizKey");
             int status = (int) params.get("auditStatus");
+            PlanCheckList plan = planManagementService.getById(planId);
             if (PlanStatusEnum.FILE.getCode() == status && StringUtils.isNotBlank(planId)) {
-                PlanCheckList plan = planManagementService.getById(planId);
                 plan.setStatus(String.valueOf(PlanStatusEnum.FILE.getCode()));
+                planManagementService.updateById(plan);
+            } else if (PlanStatusEnum.DELAY_APPLY.getCode() == status && StringUtils.isNotBlank(planId)) {
+                plan.setStatus(String.valueOf(PlanStatusEnum.DELAY_APPLY.getCode()));
+                String date = params.get("delayDate").toString();
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                plan.setDelayDate(sdf.parse(date));
+                planManagementService.updateById(plan);
+            } else if (PlanStatusEnum.COMPLETE_TOBE_AUDIT.getCode() == status && StringUtils.isNotBlank(planId)) {
+                plan.setStatus(String.valueOf(PlanStatusEnum.DELAY_APPLY.getCode()));
+                if(null != params.get("delayDate")){
+                    String date = params.get("delayDate").toString();
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                    plan.setDelayDate(sdf.parse(date));
+                }
+
+                if(params.get("delayOpt").equals("reject")){
+                    plan.setDelayDate(null);
+                }
                 planManagementService.updateById(plan);
             }
             params.remove("bizKey");
@@ -101,6 +120,7 @@ public class TaskController {
 
     /**
      * 获取流程图
+     *
      * @param processDefKey
      * @param businessId
      * @return
