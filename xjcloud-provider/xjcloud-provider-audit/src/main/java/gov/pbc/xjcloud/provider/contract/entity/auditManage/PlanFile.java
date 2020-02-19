0b13b1a3ef7f0fea7b9e5940534f5149e0df9392
paungmiao <paungmiao@163.com>
2020-02-19 10:36:57 +0800

    update

--- xjcloud-provider/xjcloud-provider-audit/src/main/java/gov/pbc/xjcloud/provider/contract/entity/auditManage/PlanFile.java
+++ xjcloud-provider/xjcloud-provider-audit/src/main/java/gov/pbc/xjcloud/provider/contract/entity/auditManage/PlanFile.java
@@ -1,6 +1,8 @@
 package gov.pbc.xjcloud.provider.contract.entity.auditManage;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -13,7 +15,7 @@ public class PlanFile {
     private String id;
 
     private String fileUri;
-
+    @JsonFormat(pattern="yyyy-MM-dd hh:mm:ss",timezone="GMT+8")
     private Date createdTime;
     /**
      * username
