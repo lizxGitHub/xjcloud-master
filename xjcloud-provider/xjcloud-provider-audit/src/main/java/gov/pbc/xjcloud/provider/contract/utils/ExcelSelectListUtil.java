274d568b113166bf19a0c9fc54a1b4699cc0988b
paungmiao <paungmiao@163.com>
2020-03-15 17:27:28 +0800

    poi工具类添加

---
+++ xjcloud-provider/xjcloud-provider-audit/src/main/java/gov/pbc/xjcloud/provider/contract/utils/ExcelSelectListUtil.java
@@ -0,0 +1,38 @@
+package gov.pbc.xjcloud.provider.contract.utils;
+
+import org.apache.poi.hssf.usermodel.DVConstraint;
+import org.apache.poi.hssf.usermodel.HSSFDataValidation;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellRangeAddressList;
+
+/**
+ * excel 导出设置下拉选项
+ *
+ * @author paungmiao@163.com
+ */
+public final class ExcelSelectListUtil {
+
+    /**
+     * firstRow 開始行號 根据此项目，默认为2(下标0开始)
+     * lastRow  根据此项目，默认为最大65535
+     * firstCol 区域中第一个单元格的列号 (下标0开始)
+     * lastCol 区域中最后一个单元格的列号
+     * strings 下拉内容
+     */
+    public static void selectList(Workbook workbook, int firstCol, int lastCol, String[] strings) {
+
+
+        Sheet sheet = workbook.getSheetAt(0);
+        //  生成下拉列表
+        //  只对(x，x)单元格有效
+        CellRangeAddressList cellRangeAddressList = new CellRangeAddressList(2, 65535, firstCol, lastCol);
+        //  生成下拉框内容
+        DVConstraint dvConstraint = DVConstraint.createExplicitListConstraint(strings);
+        HSSFDataValidation dataValidation = new HSSFDataValidation(cellRangeAddressList, dvConstraint);
+        //  对sheet页生效
+        sheet.addValidationData(dataValidation);
+
+    }
+
+}
