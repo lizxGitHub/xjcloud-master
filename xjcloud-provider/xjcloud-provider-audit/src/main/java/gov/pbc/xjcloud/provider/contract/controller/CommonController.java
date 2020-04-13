a9e79d3b55589783501c847fe8bd0b173e91c040
paungmiao <paungmiao@163.com>
2020-04-14 01:32:42 +0800

    update

--- xjcloud-provider/xjcloud-provider-audit/src/main/java/gov/pbc/xjcloud/provider/contract/controller/CommonController.java
+++ xjcloud-provider/xjcloud-provider-audit/src/main/java/gov/pbc/xjcloud/provider/contract/controller/CommonController.java
@@ -1,14 +1,19 @@
 package gov.pbc.xjcloud.provider.contract.controller;
 
+import com.baomidou.mybatisplus.extension.enums.ApiErrorCode;
+import gov.pbc.xjcloud.provider.contract.feign.user.UserCenterService;
 import gov.pbc.xjcloud.provider.contract.utils.R;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 
@@ -25,6 +30,9 @@ public class CommonController {
     @Autowired
     private DiscoveryClient discoveryClient;
 
+    @Resource
+    private UserCenterService userCenterService;
+
     @Value("${audit.gate-way.service-id:xjcloud-gateway}")
     private String gateServiceId;
 
@@ -36,6 +44,16 @@ public class CommonController {
         String gateway = serviceInstance.getUri().toString();
         return new R<>(gateway);
 
+    }
+
+    @GetMapping("/depts/{id}")
+    public R dept(@PathVariable Integer id){
+        try {
+            return userCenterService.dept(id);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new R().setCode(0).setData(new HashMap<>());
     }
 
 }
